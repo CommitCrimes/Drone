104e2ebf39e1b638264fa52af6870bedd3fed12e
uvk9ifqsqax3a67 <aurelien.maury@epitech.eu>
2025-07-22 16:12:53 +0200

    ajout info batterie

--- get_flight_info.py
+++ get_flight_info.py
@@ -13,6 +13,16 @@ def flight_info(drone_id, master):
     heartbeat = master.recv_match(type='HEARTBEAT', blocking=True, timeout=5)
     msg = master.recv_match(type='GLOBAL_POSITION_INT', blocking=True, timeout=5)
 
+    sys_status = master.recv_match(type='SYS_STATUS', blocking=False)
+    if sys_status:
+        voltage = sys_status.voltage_battery / 1000.0  # en volts
+        current = sys_status.current_battery / 100.0   # en ampères
+        battery_remaining = sys_status.battery_remaining  # en %
+    else:
+        voltage = None
+        current = None
+        battery_remaining = None
+
     if msg and heartbeat:
         vx = msg.vx / 100.0  # m/s
         vy = msg.vy / 100.0  # m/s
@@ -27,7 +37,10 @@ def flight_info(drone_id, master):
             "altitude_m": msg.relative_alt / 1000.0,
             "horizontal_speed_m_s": round(math.sqrt(vx**2 + vy**2), 2),
             "vertical_speed_m_s": round(vz, 2),
-            "heading_deg": msg.hdg / 100.0
+            "heading_deg": msg.hdg / 100.0,
+            # "battery_voltage_V": voltage,
+            # "battery_current_A": current,
+            "battery_remaining_percent": battery_remaining
         }
     else:
         raise RuntimeError("Aucune donnée de position ou heartbeat reçue.")
